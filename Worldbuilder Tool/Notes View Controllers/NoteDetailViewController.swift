729f66dc8f3c86f382bbe23e0700996a4f956a5d
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-11-01 16:22:05 -0700

    final version as of presentation at end of project week 3

---
+++ Worldbuilder Tool/Notes View Controllers/NoteDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NoteDetailViewController.swift
+//  Worldbuilder Tool
+//
+//  Created by Jonathan T. Miles on 11/1/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+import UIKit
+
+class NoteDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
